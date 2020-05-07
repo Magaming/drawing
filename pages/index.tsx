11a7a94b42b578734dd125be409ce15d1dba7ee8
Magaming <mr.magaming@gmail.com>
2020-05-07 19:29:34 +0900

    イベント変えてみる

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ const Canvas = () => {
     const ctx = getContext();
     ctx.fillStyle = "#A0A0A0A0"; //筆に白い絵の具をつけて
     ctx.fillRect(0, 0, 600, 600); //四角を描く
+    ctx.canvas.
   }
 
   const startDrawing = (x:number, y:number) => {
@@ -42,9 +43,9 @@ const Canvas = () => {
       ref={canvasRef}
       width="500px"
       height="500px"
-      onTouchStart={e => startDrawing(e.touches[0].clientX, e.touches[0].clientY)}
+      onTouchStart={e => startDrawing(e.changedTouches[0].clientX, e.changedTouches[0].clientY)}
       onTouchEnd={() => setDrawing(false)}
-      onTouchMove={e => draw(e.touches[0].clientX, e.touches[0].clientY)}
+      onTouchMove={e => draw(e.changedTouches[0].clientX, e.changedTouches[0].clientY)}
       onMouseDown={e => startDrawing(e.nativeEvent.offsetX, e.nativeEvent.offsetY)}
       onMouseUp={() => setDrawing(false)}
       onMouseLeave={() => setDrawing(false)}
